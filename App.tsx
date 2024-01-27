6e59806f6d9c05c9d17da7afb650608ac50fc5c1
Mark Faust <markfaust123@gmail.com>
2024-01-27 09:15:14 -0500

    updated App root to actually display Authentication menus for sign up or sign in

--- App.tsx
+++ App.tsx
@@ -1,20 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import { Colors } from "./src/constants/styles";
+import { store } from "./src/store/store";
+import { useAppDispatch, useAppSelector } from "./src/hooks/use-redux";
+import IconButton from "./src/components/ui/IconButton";
+import { authenticate, logout } from "./src/store/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AuthenticatedStack() {
+  const dispatch = useAppDispatch();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              size={24}
+              onPress={() => {
+                dispatch(logout());
+              }}
+            />
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  const isAuthenticated = useAppSelector(
+    (state) => state.authState.isAuthenticated
+  );
+
+  return (
+    <NavigationContainer>
+      {!isAuthenticated && <AuthStack />}
+      {isAuthenticated && <AuthenticatedStack />}
+    </NavigationContainer>
+  );
+}
+
+const Root = () => {
+  const [isTryingLogin, setIsTryingLogin] = useState<boolean>(true);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const storedToken = await AsyncStorage.getItem("token");
+        if (storedToken) {
+          dispatch(authenticate({ token: storedToken }));
+        }
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsTryingLogin(false);
+      }
+    };
+    fetchToken();
+  }, []);
+
+  if (!isTryingLogin) {
+    SplashScreen.hideAsync();
+  }
+
+  return <Navigation />;
+};
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Provider store={store}>
+        <Root />
+      </Provider>
+    </>
+  );
+}
