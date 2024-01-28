c75e19140f3626158395e7d9668d5a7c0bbad1b1
Mark Faust <markfaust123@gmail.com>
2024-01-28 10:09:34 -0500

    added dummy data for ITEMS

---
+++ src/lib/data.ts
@@ -0,0 +1,49 @@
+import type { Item } from "../types/items-types";
+
+export const ITEMS: Item[] = [
+  {
+    id: "e1",
+    name: "Shoes",
+    quantity: 1,
+    price: 59.99,
+    description: "A pair of shoes",
+    createdAt: "2023-12-19",
+    updatedAt: "2023-12-19",
+  },
+  {
+    id: "e2",
+    name: "Trousers",
+    quantity: 3,
+    price: 89.29,
+    description: "A pair of trousers",
+    createdAt: "2024-01-05",
+    updatedAt: "2024-01-05",
+  },
+  {
+    id: "e3",
+    name: "Bananas",
+    quantity: 6,
+    price: 5.99,
+    description: "Some bananas",
+    createdAt: "2023-12-01",
+    updatedAt: "2023-12-01",
+  },
+  {
+    id: "e4",
+    name: "Book #1",
+    quantity: 2,
+    price: 14.99,
+    description: "A book",
+    createdAt: "2024-01-14",
+    updatedAt: "2024-01-14",
+  },
+  {
+    id: "e5",
+    name: "Book #2",
+    quantity: 4,
+    price: 18.59,
+    description: "Another book",
+    createdAt: "2024-01-14",
+    updatedAt: "2024-01-14",
+  },
+];
