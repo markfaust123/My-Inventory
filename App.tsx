b1edd868649617a9cab1f02f147acd033cb18e12
Mark Faust <markfaust123@gmail.com>
2024-02-10 07:13:04 -0500

    added basic functionality for BottomTabsNavgiator in App.tsx. Still need to install the navigator and create the appropriate screen components

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import WelcomeScreen from "./src/screens/WelcomeScreen";
 import { Colors } from "./src/constants/styles";
 import { store } from "./src/store/store";
 import { useAppDispatch, useAppSelector } from "./src/hooks/use-redux";
-import IconButton from "./src/components/ui/IconButton";
+import AuthIconButton from "./src/components/auth/ui/IconButton";
+import ItemIconButton from "./src/components/items/ui/IconButton";
 import { authenticate, logout } from "./src/store/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
@@ -17,6 +18,57 @@ import * as SplashScreen from "expo-splash-screen";
 SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={({ navigation }: { navigation: any }) => ({
+        headerRight: ({ tintColor }) => (
+          <ItemIconButton
+            icon="add"
+            color={tintColor}
+            size={24}
+            onPress={() => {
+              navigation.navigate("ManageExpenses");
+            }}
+          />
+        ),
+        headerStyle: {
+          backgroundColor: Colors.primary500,
+        },
+        headerTintColor: "white",
+        tabBarStyle: {
+          backgroundColor: Colors.primary500,
+        },
+        tabBarActiveTintColor: Colors.accent500,
+      })}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+};
 
 function AuthStack() {
   return (
@@ -39,25 +91,32 @@ function AuthenticatedStack() {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
+        headerStyle: {
+          backgroundColor: Colors.primary500,
+        },
         headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.primary100 },
+        headerRight: ({ tintColor }) => (
+          <AuthIconButton
+            icon="exit"
+            color={tintColor}
+            size={24}
+            onPress={() => {
+              dispatch(logout());
+            }}
+          />
+        ),
       }}
     >
       <Stack.Screen
-        name="Welcome"
-        component={WelcomeScreen}
+        name="OverviewExpenses"
+        component={ExpensesOverview}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ManageExpenses"
+        component={ManageExpenses}
         options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={() => {
-                dispatch(logout());
-              }}
-            />
-          ),
+          presentation: "modal",
         }}
       />
     </Stack.Navigator>
